d4cf690962354a2e5bde2dd6dc02176127315050
Moeyjac <jo3ymac@gmail.com>
2023-05-25 21:43:56 -0400

    feat: create new storage objects and retrieve storage by index

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -10,5 +10,13 @@ contract StorageFactory{
         simpleStorageArray.push(new SimpleStorage());
     }
 
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256){
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+
 }
 
